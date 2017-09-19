4fd478be1ba87fed47b655879632a66b5a0f16a5
johnrmurray <31353797+johnrmurray@users.noreply.github.com>
2017-09-18 22:39:50 -0700

    Add files via upload
    Please Review @toropeza @ShayRomayo, @dbrizan

---
+++ InsertionSort.java
@@ -0,0 +1,21 @@
+package p03;
+
+public class InsertionSort implements SortingAlgorithm {
+	
+	public void sort(int[] a) {
+		for (int i=1; i < a.length; i++) {
+			int temp = a[i];
+			int j= i - 1;
+			while (j >= 0 && temp < a[j]) {
+				a[j+1] = a[j];
+				j--;
+			}
+			a[j+1] = temp;
+		}
+//		for (int i = 0; i < a.length; i++) {
+//			System.out.println(a[i] + " ");
+//		}
+	}
+	
+
+}
